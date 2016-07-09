83405fa734bf3c85280b4b9cb888824e4a5641fc
umbertomartinati <ugomarti@gmail.com>
2016-07-09 16:29:56 +0200

    Filtro Blur

--- prove codice/ImageFilter.cpp
+++ prove codice/ImageFilter.cpp
@@ -9,7 +9,7 @@
   ImageFilter::ImageFilter(unsigned char* bitMap,int altezza,int lunghezza){
     this->bitMap=bitMap;
     this->altezza=altezza;
-    this->larghezza=larghezza;
+    this->lunghezza=lunghezza;
   }
   
   
@@ -19,7 +19,7 @@
     double fattore=(259*(valore+255))/(255*(259-valore));
     unsigned char pixel;
     
-    for(int i=0; i<this->altezza*this->larghezza;i++){
+    for(int i=0; i<this->altezza*this->lunghezza;i++){
     
       pixel=bitMap[i];
       
@@ -43,7 +43,7 @@
     unsigned char pixel;
     
  
-    for(int i=0; i<this->altezza*this->larghezza;i++){
+    for(int i=0; i<this->altezza*this->lunghezza;i++){
     
       pixel=bitMap[i];
       pixel=(valore*pixel)/255;
@@ -67,7 +67,7 @@
     
     if(valore!=0){
       double gamma= 1/valore;
-      for(int i=0; i<this->altezza*this->larghezza;i++){
+      for(int i=0; i<this->altezza*this->lunghezza;i++){
 	pixel=bitMap[i];
 	pixel=pow(pixel/255.0f, gamma);
 	pixel=pixel*255.0f;
@@ -93,7 +93,7 @@
     
     unsigned char pixel;
     
-    for(int i=0; i<this->altezza*this->larghezza;i++){
+    for(int i=0; i<this->altezza*this->lunghezza;i++){
     
       pixel=bitMap[i];
       pixel=255-pixel;
@@ -114,7 +114,7 @@
     
     unsigned char pixel;
     
-    for(int i=0; i<this->altezza*this->larghezza;i++){
+    for(int i=0; i<this->altezza*this->lunghezza;i++){
     
       pixel=bitMap[i];
       if(pixel<=soglia)
@@ -132,6 +132,73 @@
     return bitMap;
     
   }
+  
+  
+  int ImageFilter::apply3x3Convolution(double kernel[3][3], int i, int j){
+    
+   int a=-1,b=-1,c=1,d=1;
+   
+   /*
+    * Inizio i check sulla posizione attuale. 
+    * Se sono sul bordo non posso applicare tutta la matrice di Convoluzione
+    * Si noti che un BMP inizia da basso a sinistra e finisce in alto a destra
+    */
+   
+   //Controllo se sono sulla riga in basso a sinistra
+  if(i==0)
+    a=0;
+   //Controllo se mi trovo all'inizio di una riga
+  if(j==0)
+    b=0;
+  //Controllo se mi trovo sull'ultima riga in alto
+  if(i==this->altezza-1)
+    c=0;
+  //Controllo se sono alla fine di una riga
+  if(j==this->lunghezza-1)
+    d=0;
+  
+  int ris=0,val=0;
+  
+  for(a;a<c;a++){
+   for(b;b<d;b++){
+     val=bitMap[((i+a)*this->lunghezza)+(j+b)];
+     ris += val*kernel[a][b];
+   }    
+  }
+  
+  return ris;
+    
+    
+  }
+ 
+ 
+ 
+ unsigned char* ImageFilter::blur(){
+
+   double kernel[3][3] ={
+     {1,1,1},
+     {1,1,1},
+     {1,1,1}
+ };
+ 
+    for(int i = 0;i < this->altezza;i++) {
+	for(int j = 0;j < this->lunghezza;j++) {
+	  bitMap[i*this->lunghezza+j] = apply3x3Convolution(kernel,i,j);
+	}
+    }
+    
+    return bitMap;
+   
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+  
   
   
   
--- prove codice/ImageFilter.h
+++ prove codice/ImageFilter.h
@@ -5,7 +5,10 @@ class ImageFilter{
 private:
   
    unsigned char* bitMap;
-   int altezza,larghezza;
+   int altezza,lunghezza;
+   
+   int apply3x3Convolution(double kernel[3][3],int,int);
+   
    
 public:
   
@@ -15,5 +18,9 @@ public:
   unsigned char* contrasto(double);
   unsigned char* coloreInverso();
   unsigned char* solarise(double);
+  
+  unsigned char* blur();
+  
+
   
 };
